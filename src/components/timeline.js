233d9845a4bf67f97a9f82fdd0ee5f71a0e36957
ha-nagisa <nagisa18222@icloud.com>
2021-07-19 16:17:23 +0900

    firebaseのwhereの制限を解消

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,32 +1,42 @@
 /* eslint-disable no-nested-ternary */
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import LoggedInUserContext from '../context/logged-in-user';
 import usePhotos from '../hooks/use-photos';
-import { getPhotos } from '../services/firebase';
 import Post from './post';
 
 export default function Timeline() {
   const { user } = useContext(LoggedInUserContext);
-  const { photos, setPhotos, latestDoc, setLatestDoc } = usePhotos(user);
+  const { photos } = usePhotos(user);
+  const [displayPhotos, setDisplayPhotos] = useState(null);
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
+  const [isReachingEnd, setIsReachingEnd] = useState(false);
   const isDisplayMoreRead = photos ? photos.length > 5 : false;
-  const isReachingEnd = latestDoc === undefined;
 
-  const addNextPhoto = async (doc) => {
-    setIsLoading(true);
-    if (latestDoc) {
-      const { photosWithUserDetails: nextPhotos, lastDoc } = await getPhotos(user?.userId, user?.following, doc);
-      setLatestDoc(lastDoc);
-      nextPhotos.sort((a, b) => (a.dateCreated > b.dateCreated ? -1 : 1));
-      setPhotos((prevPhotos) => [...prevPhotos, ...nextPhotos]);
+  useEffect(() => {
+    if (photos && page === 1) {
+      setDisplayPhotos(photos.filter((_, key) => key < 6));
     }
+  }, [photos]);
+  console.log(photos);
+
+  const addNextPhoto = () => {
+    console.log('こんにちは');
+    setIsLoading(true);
+    const nextPage = page + 1;
+    setPage(nextPage);
+    setDisplayPhotos((prev) => {
+      const nextPhotos = [...prev, ...photos.filter((e, key) => 6 * nextPage - 7 < key && 6 * nextPage > key)];
+      setIsReachingEnd(prev.length === nextPhotos.length);
+      return nextPhotos;
+    });
     setIsLoading(false);
   };
 
   return (
     <>
-      {photos === null ? (
+      {displayPhotos === null ? (
         <>
           <div className="sm:col-span-2 col-span-4">
             <Skeleton count={4} height={400} className="mb-5" />
@@ -35,17 +45,17 @@ export default function Timeline() {
             <Skeleton count={4} height={400} className="mb-5" />
           </div>
         </>
-      ) : photos.length === 0 ? (
+      ) : displayPhotos.length === 0 ? (
         <div className="col-span-4 text-xl text-center pt-8">フォローしているユーザーの投稿はありません。気になるユーザーをフォローしよう！</div>
       ) : (
         <>
-          {photos.map((content) => (
+          {displayPhotos.map((content) => (
             <Post key={content.docId} content={content} />
           ))}
           {isDisplayMoreRead ? (
             <div className="col-span-4 text-center pb-10 pt-5">
               <button
-                onClick={() => addNextPhoto(latestDoc)}
+                onClick={addNextPhoto}
                 type="button"
                 className={`font-bold border text-gray-700 border-gray-700 px-3 py-2 rounded-md ${
                   isLoading || isReachingEnd ? 'opacity-50 cursor-default' : 'hover:text-white hover:bg-gray-700'
